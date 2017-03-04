9ce24b41d05b0c434e3c34778ef37008e76bc4ba
Ubuntu <iamandrewau@gmail.com>
2017-03-04 23:51:40 +0000

    changed the directories for mkdir

--- bin/install.sh
+++ bin/install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-rm -rf /var/www/html/mock
+rm -rf /var/www/clubmanner.com/mock
 
-mkdir -p /var/www/html/mock
+mkdir -p /var/www/clubmanner.com/mock/a/css
+mkdir -p /var/www/clubmanner.com/mock/a/js
+mkdir -p /var/www/clubmanner.com/mock/a/fonts
 
-cp -r mock/* /var/www/html/mock
+cp -r mock/* /var/www/clubmanner.com/mock
